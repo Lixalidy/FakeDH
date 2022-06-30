3f8739ac2b0d3e51f61b3759556d20f10c1b6a18
school moment <86823692+Lixalidy@users.noreply.github.com>
2022-06-30 19:48:20 -0400

    Update prem.lua

--- prem.lua
+++ prem.lua
@@ -1,5 +1,5 @@
 getgenv().AdminTable = {
     [472813752] = {["Access"] = 5}, --owner
-    [3416511453] = {["Access"] = 5}, --alt123456789104
+    [3416511453] = {["Access"] = 5} --alt123456789104
     [1] = {["Access"] = 5} --a test acc
 }
