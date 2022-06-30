9685ac6950eceac60f129cf602eeaca6b67bb01c
school moment <86823692+Lixalidy@users.noreply.github.com>
2022-06-30 12:14:14 -0400

    Update prem.lua

--- prem.lua
+++ prem.lua
@@ -1,3 +1,5 @@
+--you can skid if you want wont impress anyone tho lmafo
 getgenv().AdminTable = {
-  [472813752] = {["Access"] = 5};
+  [472813752] = {["Access"] = 5}; --owner
 }
+--might add friends soon ngl xd 
