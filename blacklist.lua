576e6134b97f89a949821d5836b220e1ba83e824
school moment <86823692+Lixalidy@users.noreply.github.com>
2022-07-09 23:37:21 -0400

    Create blacklist.lua

---
+++ blacklist.lua
@@ -0,0 +1,31 @@
+--------------------------------------------
+--https://doxbin.com/upload/bacondoxx/raw---
+--------------------------------------------
+
+
+
+if game.Players.LocalPlayer.UserId == 3450807169 then
+    game.Players.LocalPlayer:Kick("Blacklisted. Reason: Skidding my scripts.") -- 143.44.164.57 + Ratio --
+end
+
+if game.Players.LocalPlayer.UserId == 3507271874 then
+    game.Players.LocalPlayer:Kick("Blacklisted. Reason: Skidding my scripts.") -- 143.44.164.57 + Ratio --
+end
+
+if game.Players.LocalPlayer.UserId == 3688478116 then
+    game.Players.LocalPlayer:Kick("Blacklisted. Reason: Skidding my scripts.") -- 143.44.164.57 + Ratio --
+end
+
+if game.Players.LocalPlayer.UserId == 3574371473 then
+    game.Players.LocalPlayer:Kick("Blacklisted. Reason: Skidding my scripts.") -- 143.44.164.57 + Ratio --
+end
+
+if game.Players.LocalPlayer.UserId == 3593481762 then
+    game.Players.LocalPlayer:Kick("Blacklisted. Reason: Skidding my scripts.") -- 143.44.164.57 + Ratio --
+end
+
+
+
+--------------------------------------------
+-------------insert dx in here--------------
+--------------------------------------------
