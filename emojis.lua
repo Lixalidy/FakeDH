cae0f0a7ab7027755286a65e6898e59182d6e9dd
school moment <86823692+Lixalidy@users.noreply.github.com>
2022-07-17 09:21:51 -0400

    Create emojis.lua

---
+++ emojis.lua
@@ -0,0 +1,53 @@
+local StarId = {
+	[1] = false,
+}
+
+local CrownId = {
+	[472813752] = true,
+}
+
+local DiamondId = {
+	[1] = false,
+}
+
+local OwnerId = {
+	[1] = false,
+} 
+
+function emoji()
+	for _,v in pairs(game:GetService('Players'):GetChildren()) do
+		if StarId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
+				end
+			end
+		elseif CrownId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🥵]'..v.DisplayName)
+				end
+			end
+		elseif DiamondId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]'..v.DisplayName)
+				end
+			end
+		elseif OwnerId[v.UserId] then
+			if v.Character then
+				if v.Character.Parent.Name == 'Players' then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[OMG OWNER] '..v.DisplayName)
+				end
+			end
+		elseif v.Character then
+			if v.Character.Parent.Name == 'Players' then
+				if not v.Character.UpperTorso:FindFirstChild('BodyBackAttachment') then
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[RAYX USER] '..v.DisplayName)
+				end
+			end
+		end
+	end
+end
+local success,err = pcall(emoji)
+return StarId
